a29c3964f614c6411c01899fcb4db34413b276b6
Joshua Bernardino <jbernardino7@hotmail.com>
2022-06-10 07:17:59 -0700

    adding vending machine class

---
+++ capstone/src/main/java/com/techelevator/view/VendingMachine.java
@@ -0,0 +1,55 @@
+package com.techelevator.view;
+
+public class VendingMachine {
+
+    private int overallStock;
+    public int coin;
+    public int cash;
+
+    public VendingMachine() {
+
+    }
+
+    public void getBalance() {
+
+    }
+
+    public void getMenu () {
+
+    }
+
+    //    public boolean isInStock(int stock) {
+//        if(stock < 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+
+//    public int getStock(String slotNumber) {
+//    if(isInStock) {
+//        return slotNumber.whateverthestockmethodis;
+//    } else {
+//        return 0;
+//    }
+
+//    public int getChange(int price, int cash, int coins) {
+//        int change = 0;
+//        change = (cash + coins) - price;
+//        return change;
+//    }
+
+    public void returnMainMenu(){
+
+    }
+
+    public void closeTransaction() {
+
+    }
+
+
+    public int getOverallStock() {
+        return overallStock;
+    }
+
+}
